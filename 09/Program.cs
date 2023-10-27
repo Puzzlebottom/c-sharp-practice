57c0a7d425e24217b50763400ccba3a8d0898169
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-10-26 21:13:37 -0400

    adds new exercise

---
+++ 09/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+This code reverses a string and counts the number of times a particular character appears
+*/
+string message = "The quick brown fox jumps over the lazy dog.";
+char letterToCount = 'o';
+
+char[] letters = message.ToCharArray();
+int letterCount = 0;
+
+foreach (char letter in letters)
+{
+  if (letter == letterToCount)
+  {
+    letterCount++;
+  }
+}
+
+Array.Reverse(letters);
+
+Console.WriteLine(letters);
+Console.WriteLine($"'{letterToCount}' appears {letterCount} times.");
