ce505d328afdd8c1211c27d7a53e23b90bf17775
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-10-30 10:14:24 -0400

    addes new exercise

---
+++ 14/Program.cs
@@ -0,0 +1,14 @@
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+
+bool isAdmin = permission.Contains("Admin");
+bool isManager = permission.Contains("Manager");
+string super = level > 55 ? "Super " : " ";
+
+if (level < 20 || (!isAdmin && !isManager))
+  Console.WriteLine("You do not have sufficient privileges.");
+else if (isAdmin)
+  Console.WriteLine($"Welcome,{super}Admin user.");
+else if (isManager)
+  Console.WriteLine("Contact an Admin for access.");
