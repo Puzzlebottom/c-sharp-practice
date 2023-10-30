b203ff788f817ad511013b51e205a0ea3dc1c10d
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-10-30 12:06:20 -0400

    adds new exercise

---
+++ 23/Program.cs
@@ -0,0 +1,20 @@
+﻿int heroHealth = 10;
+int monsterHealth = 10;
+Random attack = new Random();
+int damage;
+
+do
+{
+  damage = attack.Next(1, 11);
+  monsterHealth -= damage;
+  Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+
+  if (monsterHealth <= 0) continue;
+
+  damage = attack.Next(1, 11);
+  heroHealth -= damage;
+  Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");
+
+} while (heroHealth > 0 && monsterHealth > 0);
+
+Console.WriteLine(monsterHealth <= 0 ? "Hero Wins!" : "Monster Wins!");
