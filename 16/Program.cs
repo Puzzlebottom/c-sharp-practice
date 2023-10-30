cc5fbc64cf36d6178195df003067b441e3c5b83b
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-10-30 10:54:51 -0400

    adds new exercise

---
+++ 16/Program.cs
@@ -0,0 +1,13 @@
+﻿bool flag = true;
+if (flag)
+  Console.WriteLine(flag);
+
+
+string name = "steve";
+
+if (name == "bob")
+  Console.WriteLine("Found Bob");
+else if (name == "steve")
+  Console.WriteLine("Found Steve");
+else
+  Console.WriteLine("Found Chuck");
