8b6f2559d8dffb659971979b8e1539b2a89b0ed5
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-10-31 17:25:33 -0400

    adds new exercise

---
+++ 28/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string message = "";
+decimal total = 0;
+
+foreach (var value in values)
+{
+  decimal number;
+  if (decimal.TryParse(value, out number))
+    total += number;
+  else
+    message += value;
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
