ec59a0c9d035f6d26478437ffd34eaf88a9191e3
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-10-31 20:03:46 -0400

    adds new exercise

---
+++ 32/Program.cs
@@ -0,0 +1,11 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string[] words = pangram.Split(" ");
+
+for (int i = 0; i < words.Length; i++)
+{
+  char[] chars = words[i].ToCharArray();
+  Array.Reverse(chars);
+  words[i] = new string(chars);
+}
+
+Console.WriteLine(String.Join(" ", words));
